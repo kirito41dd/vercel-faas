5d7a4b9a65006d57e1c8358eaec3432d5f49f05e
kirito <yesiare.cn@gmail.com>
2023-02-03 00:50:47 +0800

    nip05 support

--- handler/handler.go
+++ handler/handler.go
@@ -13,3 +13,26 @@ func Ping(c *gin.Context) {
 func ErrRouter(c *gin.Context) {
 	c.String(http.StatusBadRequest, "url err")
 }
+
+func NIP05(c *gin.Context) {
+	name2pubkey := map[string]string{
+		"kirito": "2f7caa968b0ec9bacd55a07cfaf6206aab5a62387c76303c311db949dec8bc57",
+	}
+	user := c.Query("name")
+	if v, ok := name2pubkey[user]; ok {
+		resp := NIP05Resp{}
+		resp.Names[user] = v
+		c.JSON(http.StatusOK, resp)
+	}
+	c.Status(http.StatusNotFound)
+	return
+}
+
+type NIP05Resp struct {
+	Names map[string]string `json:"names"`
+}
+
+func Cors(c *gin.Context) {
+	c.Header("Access-Control-Allow-Origi", "*")
+	c.Next()
+}
